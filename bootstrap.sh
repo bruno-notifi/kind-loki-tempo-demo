b8b67fb546a037dc7c9af12ce449499c82883106
bruno-lucena <bruno.lucena@notifi.network>
2023-05-31 21:06:10 -0300

    master
    latest

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,15 @@
 kind create cluster --config kind.yaml
-
 # Pod errors due to “too many open files”
 # https://kind.sigs.k8s.io/docs/user/known-issues/#pod-errors-due-to-too-many-open-files
 # sudo sysctl fs.inotify.max_user_watches=524288
 # sudo sysctl fs.inotify.max_user_instances=512
 # make persistent in /etc/sysctl.conf
-
-kubectl apply -f argocd
-# kubectl create namespace argocd
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.6.8/manifests/install.yaml
-# kubectl get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" -n argocd | base64 -d && echo
-helm upgrade --install prometheus kube-prometheus-stack/kube-prometheus-stack --values prometheus/values.yaml
-# helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
-helm upgrade --install opentelemetry-collector open-telemetry/opentelemetry-collector --values otel-collector/values.yaml
-
-kubectl create namespace minio-tenant-1
-# kubectl krew update
-# kubectl krew install minio
-kubectl minio init
-kubectl apply -f minio
-# local-storage 
-# default
-# 1 1 1 1 2 parity 0
-# helm repo add grafana https://grafana.github.io/helm-charts
-helm upgrade --install --values loki/loki.yaml loki grafana/loki
-helm upgrade --install --values tempo/tempo.yaml tempo grafana/tempo
-
-# kubectl get pods -n minio-operator
-# kubectl port-forward svc/argocd-server 8001:80 -n argocd
+helm repo add prometheus https://prometheus-community.github.io/helm-charts
+helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+help repo update
+helm upgrade --install --values prometheus/values.yaml prometheus prometheus/kube-prometheus-stack
+helm upgrade --install --values otel-collector/values.yaml opentelemetry-collector open-telemetry/opentelemetry-collector
+helm upgrade --install --values promtail/values.yaml promtail grafana/promtail
+helm upgrade --install --values loki/loki.yaml loki grafana/loki-distributed
+helm upgrade --install --values tempo/values.yaml tempo grafana/tempo
--- grafana-agent-operator/README.md
+++
@@ -1,3 +0,0 @@
-will send metrics and traces to prometheus and tempo
-
-traces need to be supported. Using otel-collector instead
