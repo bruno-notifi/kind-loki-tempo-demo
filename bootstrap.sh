792f8dfd101b4af86f5dd05ed0a3c5a5050d231b
bruno-lucena <bruno.lucena@notifi.network>
2023-06-02 08:08:31 -0300

    works

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,16 @@ kind create cluster --config kind.yaml
 helm repo add prometheus https://prometheus-community.github.io/helm-charts
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
-help repo update
+helm repo update
 helm upgrade --install --values prometheus/values.yaml prometheus prometheus/kube-prometheus-stack
 helm upgrade --install --values otel-collector/values.yaml opentelemetry-collector open-telemetry/opentelemetry-collector
-helm upgrade --install --values promtail/values.yaml promtail grafana/promtail
-helm upgrade --install --values loki/loki.yaml loki grafana/loki-distributed
-helm upgrade --install --values tempo/values.yaml tempo grafana/tempo-distributed
+# helm upgrade --install --values promtail/values.yaml promtail grafana/promtail
+
+# helm upgrade --install --values loki/values.yaml loki grafana/loki-distributed
+helm upgrade --install loki grafana/loki
+
+# helm upgrade --install --values tempo/values.yaml tempo grafana/tempo-distributed --version=1.4.2
+helm upgrade --install tempo grafana/tempo
+
+kubectl delete deployment loki-grafana-agent-operator
+kubectl delete grafanaagents loki
