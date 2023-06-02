b06dae3f9b7f87aa7f42b68d85a62b04a7f4085a
bruno-lucena <bruno.lucena@notifi.network>
2023-06-02 13:38:33 -0300

    latest

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,15 +8,27 @@ helm repo add prometheus https://prometheus-community.github.io/helm-charts
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-helm upgrade --install --values prometheus/values.yaml prometheus prometheus/kube-prometheus-stack
-helm upgrade --install --values otel-collector/values.yaml opentelemetry-collector open-telemetry/opentelemetry-collector
-# helm upgrade --install --values promtail/values.yaml promtail grafana/promtail
+
+kubectl create namespace prometheus
+helm upgrade --install -n prometheus --values prometheus/values.yaml prometheus prometheus/kube-prometheus-stack --version=46.5.0
 
 # helm upgrade --install --values loki/values.yaml loki grafana/loki-distributed
-helm upgrade --install loki grafana/loki
+kubectl create namespace loki
+helm upgrade --install -n loki --values promtail/values.yaml promtail grafana/promtail --version=6.11.1
+helm upgrade --install -n loki --values loki/values.yaml loki grafana/loki --version=5.6.0
 
 # helm upgrade --install --values tempo/values.yaml tempo grafana/tempo-distributed --version=1.4.2
-helm upgrade --install tempo grafana/tempo
+kubectl create namespace tempo
+helm upgrade --install -n tempo tempo grafana/tempo --version=1.3.1
 
+kubectl create namespace otel
+helm upgrade --install -n otel --values otel-collector/values.yaml opentelemetry-collector open-telemetry/opentelemetry-collector --version=0.56.0
+
+kubectl create namespace demo
+kubectl apply -f demo
+
+# Fixes
 kubectl delete deployment loki-grafana-agent-operator
 kubectl delete grafanaagents loki
+
+# Import TNT Dashboard 16491
